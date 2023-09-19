9fdca8c11d94d23c6dccece2c91a5ad52b3eb29e
Manzimcfly <124552125+Manzimcfly@users.noreply.github.com>
2023-09-18 18:26:37 -0600

    first changes e-commerce

---
+++ src/app/discovery/page.tsx
@@ -0,0 +1,7 @@
+function DiscoveryPage() {
+  return (
+    <h1>Discovery</h1>
+  )
+}
+
+export default DiscoveryPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import styles from "./page.module.css";
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>E-commerce</h1>
-      <h2>Home</h2>
+    <main className={styles.bg}>
+      <div className={styles.cardposition}>
+      <div className={styles.cardbg}>
+      <h1 className={styles.maintitle}>E-commerce</h1>
+      <p className={styles.p}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur maxime totam quidem perferendis rem debitis nostrum culpa quia temporibus accusamus tenetur nulla, nisi, qui cupiditate vero magnam amet non at.</p>
+      </div>
+      </div>
     </main>
   );
 }
